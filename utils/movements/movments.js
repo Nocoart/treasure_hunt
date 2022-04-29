ee007f942dd30f6d8fbfa06348ad847f793a0ad7
Nicolas Guerineau <nicolasguerineau@MacBook-Air.com>
2022-04-29 18:54:01 +0200

    new test check if can move

--- utils/movements/movments.js
+++ utils/movements/movments.js
@@ -1,10 +1,8 @@
 const checkIfCanMove = (ANextPos, M, C) => {
   if (ANextPos[0] < 0 || ANextPos[1] < 0 || ANextPos[0] > C[0] || ANextPos[1] > C[1]) return false;
-
   for (let i = 0; i < M.length; i++) {
     if (M[i][0] === ANextPos[0] && M[i][1] === ANextPos[1]) return false;
   }
-
   return true;
 };
 
--- utils/movements/movments.test.js
+++ utils/movements/movments.test.js
@@ -1,13 +1,25 @@
-const { checkIfCanMove } = require("./movments");
+const { checkIfCanMove, move } = require("./movments");
 
 describe("check if can move when hit a mountain, go out the map or makes legit deplacement", () => {
+  test("should be valid move and return true", () => {
+    expect(checkIfCanMove([3, 2], [[2, 1]], [4, 5])).toBe(true);
+  });
   test("should hit a mountain and return false", () => {
     expect(checkIfCanMove([1, 1], [[1, 1]], [4, 5])).toBe(false);
   });
+  test("should hit one of the mountains and return false", () => {
+    expect(
+      checkIfCanMove(
+        [1, 1],
+        [
+          [3, 4],
+          [1, 1],
+        ],
+        [4, 5]
+      )
+    ).toBe(false);
+  });
   test("should be out of map and return false", () => {
     expect(checkIfCanMove([4, 6], [[2, 1]], [4, 5])).toBe(false);
-  });
-  test("should be valid move and return true", () => {
-    expect(checkIfCanMove([3, 2], [[2, 1]], [4, 5])).toBe(true);
   });
 });
