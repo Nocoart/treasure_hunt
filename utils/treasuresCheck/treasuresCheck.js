480d02d7cda0be52edf716b3229e5e6d24137e90
Nicolas Guerineau <nicolasguerineau@MacBook-Air.com>
2022-04-30 12:18:52 +0200

    testing treasureCheck added

--- utils/treasuresCheck/treasuresCheck.js
+++ utils/treasuresCheck/treasuresCheck.js
@@ -1,5 +1,6 @@
 const checkIfTreasure = (Aposition, hasMoved, T) => {
   if (!hasMoved) return false;
+
   for (let i = 0; i < T.length; i++) {
     if (T[i][0] === Aposition[0] && T[i][1] === Aposition[1]) return i;
   }
--- utils/treasuresCheck/treasuresCheck.test.js
+++ utils/treasuresCheck/treasuresCheck.test.js
@@ -4,4 +4,18 @@ describe("check if can move when hit a mountain, go out the map or makes legit d
   test("should not have moved and return false", () => {
     expect(checkIfTreasure([2, 1], false, [[2, 1, 2]])).toBe(false);
   });
+  test("should not be on a trasure and return false", () => {
+    expect(checkIfTreasure([2, 1], true, [[2, 2, 2]])).toBe(false);
+  });
+  test("should be on a trasure and return index 0", () => {
+    expect(checkIfTreasure([2, 1], true, [[2, 1, 2]])).toBe(0);
+  });
+  test("should be on a trasure and return index 1", () => {
+    expect(
+      checkIfTreasure([2, 1], true, [
+        [4, 1, 2],
+        [2, 1, 2],
+      ])
+    ).toBe(1);
+  });
 });
